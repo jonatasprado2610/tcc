a2337fb3e6814160e7868238263640e5c0e9d8b9
jaozinmafase <neres.julianosouzacosta@gmail.com>
2022-10-06 11:58:07 -0300

    cadastrar endereco ok

--- api/src/repository/enderecoRepository.js
+++ api/src/repository/enderecoRepository.js
@@ -2,17 +2,18 @@ import {con} from './connection.js';
 
 export async function cadastrarEndereco(endereco) {
     const comando = `insert into tb_usuario_endereco (
+        id_usuario
         DS_LOGRADOURO,
         DS_CEP,
         DS_RUA,
         DS_BLOCO,
         NR_NUMERO
     )
-    values(?, ?, ?, ?, ?)
+    values(?, ?, ?, ?, ?, ?)
     `
-    const [linhas] = await con.query(comando, [         
-                                               endereco.idUsuario,
-                                               endereco.logradouro, 
+    const [linhas] = await con.query(comando, [
+                                                endereco.idusuario,
+                                                endereco.logradouro, 
                                                endereco.cep, 
                                                endereco.rua, 
                                                endereco.bloco, 
