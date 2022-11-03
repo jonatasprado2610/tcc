5aeb6823d7e0491ce5a059b9abbdbf8a3aa5657a
J-neres <neres.julianosouzacosta@gmail.com>
2022-11-03 18:12:33 -0300

    detalhesPedido scss 85%

--- site/src/pages/detalhesPedido/index.js
+++ site/src/pages/detalhesPedido/index.js
@@ -4,6 +4,7 @@ import { Listar} from '../../api/cadastrarEndereco';
 import { useEffect, useState } from 'react';
 import storage from 'local-storage'
 import { carregarProdutosPorId } from '../../api/produtoApi';
+;
 
 
 export default function MeusPedidos() {
@@ -63,30 +64,35 @@ export default function MeusPedidos() {
 
              
 
-                    <div className='infosProd'>
-                                        <img src='https://static.allianzparqueshop.com.br/produtos/tenis-adidas-breaknet-feminino/28/NQQ-4379-028/NQQ-4379-028_zoom1.jpg?ts=1661862763&ims=544x' alt='' />
-                                        <div >
-                                            <h3>Tenis Campus Adidas</h3>
-                                            <p>Tamanho: 42</p>
-                                            <p>Cor: Preto, refletivo</p>
-                                            <id>876</id>
-                                        </div>
-                                        <div>
-                                            <h3>Realizado em 22/07/2022</h3>
-                                            <button>Confirmar entrega</button>
-                                        </div>
-                                    </div>
+                    <div className='infosProd'> 
+                        <div className='descricoes' >
+                            <img className='img-produto' src='https://static.allianzparqueshop.com.br/produtos/tenis-adidas-breaknet-feminino/28/NQQ-4379-028/NQQ-4379-028_zoom1.jpg?ts=1661862763&ims=544x' alt='' />
+                            <div>
+                                <h3>Tenis Adidas Campus hair nothion cfh</h3>
+                                <p>Tamanho: 42</p>
+                                <p>Cor: Preto, refletivo</p>
+                                <id>876</id>
+                            </div>
+                        </div>
+
+                        <div className='descricoes2'>
+                            <h3>Realizado em 22/07/2022</h3>
+                            <button className='botao-confirmar'>Confirmar entrega</button>
+                        </div>
+                    </div>
                 
                 <div className='div-cards'>
                     <div className='cards'>
-                        <h3>Valor total</h3>
-
-                        <div>
-                            <p>Valor total R$ </p>
+                    <h2 className='titulo-entrega'> VALOR </h2>
+                        <div className='div-infosende'>
+                            <h3 className='infosende'>Valor total: _________  R$249,99</h3>
+            
+                            <h3 className='infosende'>Valor total R$ </h3>
+                            
+                            <h3 className='infosende'>Frete Grátis</h3>
+            
+                            <h3 className='infosende'>Valor total</h3>
                         </div>
-                        <div>Frete Grátis</div>
-                        <br></br>
-                        <div>Valor total</div>
                     </div>
                    {/*<div className='cards'>
                         <h3>Pagamento</h3>
@@ -96,22 +102,25 @@ export default function MeusPedidos() {
                     </div>*/}
                     
                     <div className='cards'>
-                      <h3 className='titulo-entrega'>--- ENTREGA ---</h3>
+                      <h2 className='titulo-entrega'> ENTREGA </h2>
                         <div>
-                            <p>Etrega Normal</p>
+                            <p>Entrega Normal</p>
                         </div> 
 
                         {enderecos.map(item =>
                             <div classname= "div-infosende" >
-                                <h3>{item.logradouro} {item.numero} </h3>
+                                {/*PERGUNTAR PRO BRUNO*/}
+                                {/*como fazer para formatar no scss so as <h3> dessa div?? */ }
+
+                                <h3 className='infosende'>{item.logradouro} {item.numero} </h3>
                                 
-                                <h3>{item.bairro} {item.estado} {item.cidade}</h3>
+                                <h3 className='infosende'>{item.bairro} {item.estado} {item.cidade}</h3>
                                 
-                                <h3>{item.cep} </h3>
+                                <h3 className='infosende'>{item.cep} </h3>
                                                                                              
-                                <h3>{item.referencia}</h3>
+                                <h3 className='infosende'>{item.referencia}</h3>
                                                                
-                                <h3>{item.complemento}:</h3>
+                                <h3 className='infosende'>{item.complemento}:</h3>
                             </div>
                            
                             )}
