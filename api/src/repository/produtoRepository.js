e03995a4363af999b31e4c9990cf07c9385ae216
Matheusthebest <gamesmatheusplay9@gmail.com>
2022-11-07 21:39:25 -0300

    estoque 100%

--- api/src/repository/produtoRepository.js
+++ api/src/repository/produtoRepository.js
@@ -114,23 +114,20 @@ export async function salvarProdutoImagem(idProduto, imagemPath) {
 
 export async function buscarProdutos() {
     const comando = ` 
-    select  tb_produto.ID_PRODUTO    id,
+    select tb_produto.ID_PRODUTO    id,
     NM_PRODUTO                    nome,
-    select  tb_produto.
-    ID_PRODUTO    id,
-    NM_PRODUTO                     nome,
     VL_PRECO_DE                    preco,
-    VL_PRECO_POR                  precopar,
+    VL_PRECO_POR           precopar,
     QTD_ITENS              qtd,
     BL_CATEGORIA_DIARIA     destaque,
-    count(NM_CATEGORIA) as qtd_categorias,
+    count(NM_CATEGORIA)  qtd_categorias,
     NM_MARCA      marca
     from tb_produto
     inner join tb_produto_categoria on tb_produto.id_produto=tb_produto.id_produto
     inner join tb_categoria on tb_categoria.id_categoria = tb_produto_categoria.id_produto
     inner join tb_produto_marca on tb_produto_marca.ID_PRODUTO = tb_produto.ID_PRODUTO
     inner join tb_marca on tb_marca.ID_MARCA = tb_produto_marca.ID_MARCA
-    group by
+	group by
     tb_produto.ID_PRODUTO,
     NM_PRODUTO,
     VL_PRECO_DE,
